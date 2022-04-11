1502944508f5c2c839c5df132b23c36dec8da084
DisBLeep <102161796+DisBLeep@users.noreply.github.com>
2022-04-11 17:59:05 +0200

    Mostly Functions

--- Software/func.py
+++ Software/func.py
@@ -1,2 +1,55 @@
-def test():
-    print('aa')
+from cgi import print_arguments
+import random
+import os
+import time
+#from main import *
+
+debug = True
+
+def clear():
+    os.system('cls||clear')
+
+def rand_array(min, max, amount):
+    result = random.sample(range(min, max), amount)
+    #if debug:
+        #print(result)
+    return result
+
+def log(type, msg):
+    logfile = open("logfile.txt", "a")
+    logfile.write(f'{timestamp()}|{type}|{msg}\n')
+    logfile.close()
+
+def timestamp():
+    named_tuple = time.localtime() # get struct_time
+    time_string = time.strftime("%d.%m.%y|%H:%M:%S", named_tuple)
+    return time_string
+
+def askYN(ask):
+    answers = ['Y','N','EXIT']
+    user_answer = ''
+    while user_answer.upper() not in answers:
+        user_answer = input(f'{ask} (Y/N/Exit)')
+    return user_answer.upper()
+
+def let_user_choose(min, max, amount_to_choose):
+    choice_pallet = list(range(min, max))
+    choice_commit = []
+    while len(choice_commit)<amount_to_choose:
+        clear()
+        print(f'Choose from: {choice_pallet}')
+        print(f'{len(choice_commit)}/{amount_to_choose} Choosen: {choice_commit}')
+        print('\n')
+        choosen = input('Submit Number from the range above: ')
+        try:
+            choosen = int(choosen)
+            if choosen in choice_pallet:
+                choice_pallet.remove(choosen)
+                choice_commit.append(choosen)
+        finally:
+            continue
+    YN = askYN(f'Commit Numbers:{choice_commit}?')
+    if YN == 'Y':
+        return choice_commit
+    else:
+        exit
--- Software/main.py
+++ Software/main.py
@@ -1,4 +1,17 @@
+import random
 import os
 from func import *
+os.system('cls||clear')
+#lotto
+""" 3 zł 
+Wybierz 6 liczb z 49 """
+#multi multi
+""" 2,5 zł
+Wytypuj swoje liczby z zakresu od 1 do 80. """
+#eurojackpot
+""" 12,5 zł
+Wybierz 5 liczb z 50 i 2 liczby z 12 """
 
-test()
+
+x = let_user_choose(1, 80, 5)
+log('ok',x)
