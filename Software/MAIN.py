e9d7b2077f4e0599884f5f6ca175de121037b892
DisBLeep <102161796+DisBLeep@users.noreply.github.com>
2022-06-23 13:11:36 +0200

    Bugfix - duplicated logs, stacking menu's

--- Software/MAIN.py
+++ Software/MAIN.py
@@ -7,6 +7,10 @@ while True:
     clear()
     print(f'---Main Menu---\n')
     x = show_menu(menu_main)
+    if x not in list(menu_main.keys()):
+        clear()
+        continue
+
     if menu_main[x] == "Exit":
         clear()
         quit()
@@ -15,13 +19,16 @@ while True:
         showlog()
         continue
 
-    while menu_main[x] != "Exit":
+    while menu_main[x] in list(menu_main.values()):
         clear()
         game = menu_main[x]
         print(f'---{game}---  ', end="", flush=True)
         print(eval(f'{game}.Desc') + "\n") #Yeah i just make command strings and eval them, you scared?
 
         y = show_menu(menu_game)
+        if y not in list(menu_game.keys()):
+            clear()
+            continue
 
         if menu_game[y] == "Menu":
             break
@@ -33,25 +40,25 @@ while True:
 
         if menu_game[y] == "Play":
             totuser, totbank, totmatch, matched = [],[],[],0
-            rollamtcheck = eval(f'len({game}.nchs_amt)')
 
+            rollamtcheck = eval(f'len({game}.nchs_amt)')
             for rolls in range(rollamtcheck): #Just for EuroJ since you roll two indp sets in one game
 
                 min = eval(f'{game}.roll_range[{rolls}]["min"]')
                 max = eval(f'{game}.roll_range[{rolls}]["max"]')
                 amt = eval(f'{game}.nchs_amt[{rolls}]')
 
-                userticket = let_user_choose(min, max, amt)
-                bankticket = let_bank_choose(min, max, amt)
-                matched += len(set(userticket) & set(bankticket))
+                userticket  = let_user_choose(min, max, amt)
+                bankticket  = let_bank_choose(min, max, amt)
+                matched    += len(set(userticket) & set(bankticket))
 
                 totuser.append(userticket)
                 totbank.append(bankticket)
                 totmatch.append(matched)
 
-                csvwrite(game,totuser)
-                csvwrite(game,totbank,'bank')
-                csvwrite(game,totmatch,'match')
+            csvwrite(game,totuser)
+            csvwrite(game,totbank,'bank')
+            csvwrite(game,totmatch,'match')
 
             clear()
             if matched>0:
